60e502e64e87bd918a6c9f9a9329d5d0f64cdd2b
paas <paas@cse-codegen-backend-3946097040-nqqvw>
2017-12-19 13:02:37 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/hwservicestage02/Hwservicestage02Application.java
@@ -0,0 +1,11 @@
+package com.service.hwservicestage02;
+
+import io.servicecomb.foundation.common.utils.BeanUtils;
+import io.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class Hwservicestage02Application {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
